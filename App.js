fdf1ea8ce819a7cb99e12b624772fb3fbd395c77
BoonyaOnkling <boonyanutch.133@gmail.com>
2022-06-22 11:11:36 +0700

    practice2

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import { StyleSheet, Text, View, } from "react-native";
 import React from "react";
 import Logo from "./components/Logo";
-import styles from "./components/Styles";
+import Stysheet from "./components/Stysheet";
+
+
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={[styles.title,styles.warning]}>HELLO React Native</Text>
-      <Logo />
+      {/* <Text style={[styles.title,styles.warning]}>HELLO React Native</Text>
+      <Logo /> */}
+      <Stysheet/>
     </View>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+  },
+  warning: {
+    color: 'red'
+  }
+});
+
 
 
--- components/Styles.js
+++ components/Styles.js
@@ -1,18 +1,5 @@
 import { StyleSheet } from 'react-native'
 
-const styles = StyleSheet.create({
-    container: { 
-        flex: 1, 
-        justifyContent: 'center', 
-        alignContent: 'center' },
-    
-      title: {
-        fontSize: 20,
-      },
-    
-      warning:{
-        color: 'blue'
-      }
-});
+
 
 export default styles
